21a664e0310f5ab05310955249934389c71d2057
poojamalvia <poojamalvia@gmail.com>
2025-05-18 00:18:45 +0530

    new

--- src/AdminPrivateRoutes.js
+++ src/AdminPrivateRoutes.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Navigate,Outlet } from "react-router-dom";
 
 const AdminPrivateRoutes =()=>{
+     const isAuthenticated = !!localStorage.getItem("token"); // Check if token exists
+    
+      return isAuthenticated ? <Outlet /> : <Navigate to="/admin/login" />;
 
     
 }
